d8fb68dc7710b2717600f915fae1ce20481e115a
Param shah <param.s1@solutelabs.com>
2025-01-17 14:20:02 +0530

    basic methods,modules,controllers,services etc

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+
+
+    constructor(private tasksService: TasksService) { }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto:CreateTaskDto):Task {
+        return this.tasksService.createTask(createTaskDto);
+    }
+
+
+}
+
+@Controller('kids')
+export class KidsController{
+    @Get()
+    getAllKids(){
+        return "All Kids";
+    }
+}
+
